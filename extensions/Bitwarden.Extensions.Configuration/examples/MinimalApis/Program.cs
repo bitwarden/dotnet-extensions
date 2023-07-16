f2f389e882d6d803c3f15137d08f6efe31f0a9da
Justin Baur <19896123+justindbaur@users.noreply.github.com>
2023-07-16 14:32:41 -0400

    Make Example Slimmer

--- extensions/Bitwarden.Extensions.Configuration/examples/MinimalApis/Program.cs
+++ extensions/Bitwarden.Extensions.Configuration/examples/MinimalApis/Program.cs
@@ -1,27 +1,14 @@
 var builder = WebApplication.CreateBuilder(args);
+builder.Configuration.Sources.Clear();
+builder.Configuration.AddUserSecrets<Program>();
 
 builder.Configuration.AddSecretsManager(
     projectId: Guid.Parse("5aabfa15-d60b-416d-a020-b03301462b86"),
     builder.Configuration.GetValue<string>("SecretsManager:AccessToken")!,
     reloadInterval: TimeSpan.FromMinutes(1));
 
-// Add services to the container.
-
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
-app.UseHttpsRedirection();
-
-app.MapGet("/config", (IConfiguration configuration) => ((IConfigurationRoot)configuration).GetDebugView());
+app.MapGet("/", (IConfiguration configuration) => ((IConfigurationRoot)configuration).GetDebugView());
 
 app.Run();
