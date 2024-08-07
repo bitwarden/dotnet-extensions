3817bcac8764ae78919602cc59571cd7ef907aae
Justin Baur <19896123+justindbaur@users.noreply.github.com>
2024-08-06 21:48:53 -0400

    Start Generator Project

---
+++ extensions/Bitwarden.Proxy/src/BitwardenProxyGenerator.cs
@@ -0,0 +1,13 @@
+using Microsoft.CodeAnalysis;
+
+namespace Bitwarden.Proxy
+{
+    [Generator]
+    public class BitwardenProxyGenerator : IIncrementalGenerator
+    {
+        public void Initialize(IncrementalGeneratorInitializationContext context)
+        {
+            context.
+        }
+    }
+}
