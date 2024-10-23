8956cdbdcdef6de8baa0384fa2b6897ddf6abc5f
Justin Baur <19896123+justindbaur@users.noreply.github.com>
2024-10-23 04:32:27 -0400

    Update extensions/Bitwarden.Extensions.Hosting/src/Features/LoggerExtensions.cs
    Co-authored-by: Matt Bishop <mbishop@bitwarden.com>

--- extensions/Bitwarden.Extensions.Hosting/src/Features/LoggerExtensions.cs
+++ extensions/Bitwarden.Extensions.Hosting/src/Features/LoggerExtensions.cs
@@ -4,7 +4,7 @@ namespace Bitwarden.Extensions.Hosting.Features;
 
 internal static partial class LoggerExtensions
 {
-    [LoggerMessage(1, LogLevel.Warning, "No Endpoint Set, Maybe you forgot to call 'UseRouting()'.")]
+    [LoggerMessage(1, LogLevel.Warning, "No endpoint set -- did you forget to call 'UseRouting()'?")]
     public static partial void LogNoEndpointWarning(this ILogger logger);
 
     [LoggerMessage(2, LogLevel.Debug, "Failed feature check {CheckName}", SkipEnabledCheck = true)]
