dbf65acb092808a32af98034f646308bfa4a2e0d
Justin Baur <19896123+justindbaur@users.noreply.github.com>
2024-12-02 09:28:10 -0500

    Work On Release Pipeline

---
+++ scripts/UpdatePackageVersion.ps1
@@ -0,0 +1,51 @@
+<#
+
+.PARAMETER RepoRoot
+
+#>
+
+[CmdletBinding()]
+Param (
+    [ValidateNotNullOrEmpty()]
+    [string] $RepoRoot = "${PSScriptRoot}/..",
+    [Parameter(Mandatory=$True)]
+    [string] $PackageName,
+    [Parameter(Mandatory=$True)]
+    [string] $BumpType
+)
+
+$csprojPath = Join-Path $RepoRoot extensions $PackageName src "${PackageName}.csproj"
+
+$csproj = New-Object xml
+$csproj.PreserveWhitespace = $true
+$csproj.Load($csprojPath)
+$propertyGroup = ($csproj | Select-Xml "Project/PropertyGroup/VersionPrefix").Node.ParentNode
+$versionPrefix = $propertyGroup.VersionPrefix
+$currentVersion = New-Object -TypeName System.Version -ArgumentList $versionPrefix
+
+Write-Host "Current VersionPrefix: $currentVersion"
+
+Switch ($BumpType)
+{
+    "ga" {
+        # Bump the minor version and reset the build and prerelease iteration
+        $newVersion = New-Object -TypeName System.Version -ArgumentList ($currentVersion.Major, ($currentVersion.Minor+1), 0)
+        $newPreReleaseIteration = 1
+    }
+    "prerelease" {
+        # Keep the same version and bump prerelease iteration
+        $newVersion = $currentVersion
+        $newPreReleaseIteration = ([int]$propertyGroup.PreReleaseVersionIteration + 1)
+    }
+    "hotfix" {
+        # Bump just the build number and reset the prerelease iteration
+        $newVersion = New-Object -TypeName System.Version -ArgumentList ($currentVersion.Major, $currentVersion.Minor, ($currentVersion.Build + 1))
+        $newPreReleaseIteration = 1
+    }
+}
+
+Write-Host "New VersionPrefix $newVersion"
+
+$propertyGroup.VersionPrefix = $newVersion.ToString()
+$propertyGroup.PreReleaseVersionIteration = $newPreReleaseIteration
+$csproj.Save($csprojPath)
