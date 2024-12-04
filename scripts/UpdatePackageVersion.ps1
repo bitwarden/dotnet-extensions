8d3a68068159e62528304a09a2e6733db7bc3e7a
Justin Baur <19896123+justindbaur@users.noreply.github.com>
2024-12-04 14:46:29 -0500

    Release workflow (#50)
    * Add cut-release Workflow
    
    * Work On Release Pipeline
    
    * Output new version from update package version script
    
    * Create PR
    
    * Add Release Creation
    
    * Apply suggestions from code review
    
    Co-authored-by: Matt Bishop <mbishop@bitwarden.com>
    
    * Add Explicit Permissions
    
    * Cleanup New Line
    
    * Update Release Creation Comment
    
    Co-authored-by: Matt Bishop <mbishop@bitwarden.com>
    
    ---------
    
    Co-authored-by: Matt Bishop <mbishop@bitwarden.com>

---
+++ scripts/UpdatePackageVersion.ps1
@@ -0,0 +1,60 @@
+<#
+
+.PARAMETER RepoRoot
+
+#>
+
+[CmdletBinding()]
+Param (
+    [ValidateNotNullOrEmpty()]
+    [string] $RepoRoot = "${PSScriptRoot}/..",
+    [Parameter(Mandatory=$True)]
+    [string] $PackageName,
+    [Parameter(Mandatory=$True)]
+    [string] $BumpType
+)
+
+$csprojPath = Join-Path $RepoRoot extensions $PackageName src "${PackageName}.csproj"
+
+$csproj = New-Object xml
+$csproj.PreserveWhitespace = $true
+$csproj.Load($csprojPath)
+$propertyGroup = ($csproj | Select-Xml "Project/PropertyGroup/VersionPrefix").Node.ParentNode
+$versionPrefix = $propertyGroup.VersionPrefix
+$currentVersion = New-Object -TypeName System.Version -ArgumentList $versionPrefix
+
+$showPreRelease = $false
+
+Switch ($BumpType)
+{
+    "ga" {
+        # Bump the minor version and reset the build and prerelease iteration
+        $newVersion = New-Object -TypeName System.Version -ArgumentList ($currentVersion.Major, ($currentVersion.Minor+1), 0)
+        $newPreReleaseIteration = 1
+    }
+    "prerelease" {
+        # Keep the same version and bump prerelease iteration
+        $newVersion = $currentVersion
+        $newPreReleaseIteration = ([int]$propertyGroup.PreReleaseVersionIteration + 1)
+        $showPreRelease = $true
+    }
+    "hotfix" {
+        # Bump just the build number and reset the prerelease iteration
+        $newVersion = New-Object -TypeName System.Version -ArgumentList ($currentVersion.Major, $currentVersion.Minor, ($currentVersion.Build + 1))
+        $newPreReleaseIteration = 1
+    }
+}
+
+if ($showPreRelease)
+{
+    $label = $propertyGroup.PreReleaseVersionLabel;
+    Write-Host "$newVersion-$label.$newPreReleaseIteration"
+}
+else
+{
+    Write-Host "$newVersion"
+}
+
+$propertyGroup.VersionPrefix = $newVersion.ToString()
+$propertyGroup.PreReleaseVersionIteration = $newPreReleaseIteration
+$csproj.Save($csprojPath)
