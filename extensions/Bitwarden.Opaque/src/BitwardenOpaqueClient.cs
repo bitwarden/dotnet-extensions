b34c5b05ea44e97869ad1463cba9918c6e90aefb
Daniel García <dani-garcia@users.noreply.github.com>
2025-03-18 18:39:43 +0100

    Improve how the type conversions are done and add missing docs

--- extensions/Bitwarden.Opaque/src/BitwardenOpaqueClient.cs
+++ extensions/Bitwarden.Opaque/src/BitwardenOpaqueClient.cs
@@ -1,20 +1,20 @@
 ﻿namespace Bitwarden.Opaque;
 #pragma warning disable CA1822 // Mark members as static
 
-/// The result of <see cref="Bitwarden.Opaque.BitwardenOpaqueClient.StartRegistration"/>
+/// The result of <see cref="BitwardenOpaqueClient.StartRegistration"/>
 public struct ClientRegistrationStartResult
 {
-    /// The registration response which is then passed to <see cref="Bitwarden.Opaque.BitwardenOpaqueServer.StartRegistration"/>.
+    /// The registration response which is then passed to <see cref="BitwardenOpaqueServer.StartRegistration"/>.
     public byte[] registrationRequest;
 
-    /// The client state, which must be kept on the client for <see cref="Bitwarden.Opaque.BitwardenOpaqueClient.FinishRegistration"/>.
+    /// The client state, which must be kept on the client for <see cref="BitwardenOpaqueClient.FinishRegistration"/>.
     public byte[] state;
 }
 
-/// The result of <see cref="Bitwarden.Opaque.BitwardenOpaqueClient.FinishRegistration"/>
+/// The result of <see cref="BitwardenOpaqueClient.FinishRegistration"/>
 public struct ClientRegistrationFinishResult
 {
-    /// The registration upload which is then passed to <see cref="Bitwarden.Opaque.BitwardenOpaqueServer.FinishRegistration"/>.
+    /// The registration upload which is then passed to <see cref="BitwardenOpaqueServer.FinishRegistration"/>.
     public byte[] registrationUpload;
     /// The export key output by client registration
     public byte[] exportKey;
@@ -22,17 +22,25 @@ public struct ClientRegistrationFinishResult
     public byte[] serverSPKey;
 }
 
+/// The result of <see cref="BitwardenOpaqueClient.StartLogin"/>
 public struct ClientLoginStartResult
 {
+    /// The credential request which is then passed to <see cref="BitwardenOpaqueServer.StartLogin"/>.
     public byte[] credentialRequest;
+    /// The state generated during the login start, which must be kept on the client for <see cref="BitwardenOpaqueClient.FinishLogin"/>.
     public byte[] state;
 }
 
+/// The result of <see cref="BitwardenOpaqueClient.FinishLogin"/>
 public struct ClientLoginFinishResult
 {
+    /// The credential finalization which is then passed to <see cref="BitwardenOpaqueServer.FinishLogin"/>.
     public byte[] credentialFinalization;
+    /// The session key generated after a successful login.
     public byte[] sessionKey;
+    /// The export key output by client login.
     public byte[] exportKey;
+    /// The server's static public key.
     public byte[] serverSPKey;
 }
 
@@ -48,54 +56,79 @@ public sealed partial class BitwardenOpaqueClient
     /// <returns></returns>
     public ClientRegistrationStartResult StartRegistration(CipherConfiguration config, string password)
     {
-        var (registrationRequest, state) = BitwardenLibrary.StartClientRegistration(config, password);
+        var result = BitwardenLibrary.ExecuteFFIFunction((ffi) =>
+        {
+            return BitwardenLibrary.start_client_registration(ffi.Cfg(config), password);
+        }, 2);
         return new ClientRegistrationStartResult
         {
-            registrationRequest = registrationRequest,
-            state = state
+            registrationRequest = result[0],
+            state = result[1]
         };
     }
 
     /// <summary>
-    /// Finish the server registration process. This must happen after <see cref="Bitwarden.Opaque.BitwardenOpaqueServer.StartRegistration"/> 
+    /// Finish the server registration process. This must happen after <see cref="BitwardenOpaqueServer.StartRegistration"/> 
     /// </summary>
     /// <param name="config">The Cipher configuration, must be the same for all the operation</param>
-    /// <param name="state">The state obtained from the client start operation, <see cref="Bitwarden.Opaque.ClientRegistrationStartResult.state"/> </param>
-    /// <param name="registrationResponse">The server registration response, <see cref="Bitwarden.Opaque.ServerRegistrationStartResult.registrationResponse"/> </param>
+    /// <param name="state">The state obtained from the client start operation, <see cref="ClientRegistrationStartResult.state"/> </param>
+    /// <param name="registrationResponse">The server registration response, <see cref="ServerRegistrationStartResult.registrationResponse"/> </param>
     /// <param name="password">The password to register</param>
     /// <returns></returns>
     public ClientRegistrationFinishResult FinishRegistration(CipherConfiguration config, byte[] state, byte[] registrationResponse, string password)
     {
-        var (registrationUpload, exportKey, serverSPKey) = BitwardenLibrary.FinishClientRegistration(config, state, registrationResponse, password);
+        var result = BitwardenLibrary.ExecuteFFIFunction((ffi) =>
+        {
+            return BitwardenLibrary.finish_client_registration(ffi.Cfg(config), ffi.Buf(state), ffi.Buf(registrationResponse), password);
+        }, 3);
         return new ClientRegistrationFinishResult
         {
-            registrationUpload = registrationUpload,
-            exportKey = exportKey,
-            serverSPKey = serverSPKey
+            registrationUpload = result[0],
+            exportKey = result[1],
+            serverSPKey = result[2]
         };
     }
 
-
+    /// <summary>
+    /// Start the client login process. This is the first step in the login process.
+    /// </summary>
+    /// <param name="config">The Cipher configuration, must be the same for all the operation</param>
+    /// <param name="password">The password to login</param>
+    /// <returns></returns>
     public ClientLoginStartResult StartLogin(CipherConfiguration config, string password)
     {
-        var (credentialRequest, state) = BitwardenLibrary.StartClientLogin(config, password);
+        var result = BitwardenLibrary.ExecuteFFIFunction((ffi) =>
+        {
+            return BitwardenLibrary.start_client_login(ffi.Cfg(config), password);
+        }, 2);
         return new ClientLoginStartResult
         {
-            credentialRequest = credentialRequest,
-            state = state
+            credentialRequest = result[0],
+            state = result[1]
         };
 
     }
 
+    /// <summary>
+    /// Finish the client login process. This must happen after <see cref="BitwardenOpaqueServer.StartLogin"/> 
+    /// </summary>
+    /// <param name="config">The Cipher configuration, must be the same for all the operation</param>
+    /// <param name="state">The state obtained from the client start operation, <see cref="ClientLoginStartResult.state"/> </param>
+    /// <param name="credentialResponse">The server credential response, <see cref="ServerLoginStartResult.credentialResponse"/> </param>
+    /// <param name="password">The password to login</param>
+    /// <returns></returns>
     public ClientLoginFinishResult FinishLogin(CipherConfiguration config, byte[] state, byte[] credentialResponse, string password)
     {
-        var (credentialFinalization, sessionKey, exportKey, serverSPKey) = BitwardenLibrary.FinishClientLogin(config, state, credentialResponse, password);
+        var result = BitwardenLibrary.ExecuteFFIFunction((ffi) =>
+        {
+            return BitwardenLibrary.finish_client_login(ffi.Cfg(config), ffi.Buf(state), ffi.Buf(credentialResponse), password);
+        }, 4);
         return new ClientLoginFinishResult
         {
-            credentialFinalization = credentialFinalization,
-            sessionKey = sessionKey,
-            exportKey = exportKey,
-            serverSPKey = serverSPKey
+            credentialFinalization = result[0],
+            sessionKey = result[1],
+            exportKey = result[2],
+            serverSPKey = result[3]
         };
     }
 }
--- extensions/Bitwarden.Opaque/src/BitwardenOpaqueServer.cs
+++ extensions/Bitwarden.Opaque/src/BitwardenOpaqueServer.cs
@@ -2,16 +2,16 @@
 #pragma warning disable CA1822 // Mark members as static
 
 
-/// The result of <see cref="Bitwarden.Opaque.BitwardenOpaqueServer.StartRegistration"/>
+/// The result of <see cref="BitwardenOpaqueServer.StartRegistration"/>
 public struct ServerRegistrationStartResult
 {
-    /// The registration response which is then passed to <see cref="Bitwarden.Opaque.BitwardenOpaqueClient.FinishRegistration"/>.
+    /// The registration response which is then passed to <see cref="BitwardenOpaqueClient.FinishRegistration"/>.
     public byte[] registrationResponse;
     /// The server setup, which needs to be persisted on the server for future logins.
     public byte[] serverSetup;
 }
 
-/// The result of <see cref="Bitwarden.Opaque.BitwardenOpaqueServer.FinishRegistration"/>
+/// The result of <see cref="BitwardenOpaqueServer.FinishRegistration"/>
 public struct ServerRegistrationFinishResult
 
 {
@@ -19,70 +19,103 @@ public struct ServerRegistrationFinishResult
     public byte[] serverRegistration;
 }
 
+/// The result of <see cref="BitwardenOpaqueServer.StartLogin"/>
 public struct ServerLoginStartResult
 {
+    /// The credential response which is then passed to <see cref="BitwardenOpaqueClient.FinishLogin"/>.
     public byte[] credentialResponse;
+    /// The state generated during the login start, which needs to be stored until the login finish.
     public byte[] state;
 }
 
+/// The result of <see cref="BitwardenOpaqueServer.FinishLogin"/>
 public struct ServerLoginFinishResult
 {
+    /// The session key generated after a successful login.
     public byte[] sessionKey;
-
 }
 
 /// A class to represent server side functionality the Bitwarden OPAQUE library.
 public sealed partial class BitwardenOpaqueServer
 {
     /// <summary>
-    /// Start the server registration process. This must happen after <see cref="Bitwarden.Opaque.BitwardenOpaqueClient.StartRegistration"/> 
+    /// Start the server registration process. This must happen after <see cref="BitwardenOpaqueClient.StartRegistration"/> 
     /// </summary>
     /// <param name="config">The Cipher configuration, must be the same for all the operation</param>
     /// <param name="serverSetup">The server setup. Use null to let the library create a new random one</param>
-    /// <param name="registrationRequest">The client registration request, <see cref="Bitwarden.Opaque.ClientRegistrationStartResult.registrationRequest"/> </param>
+    /// <param name="registrationRequest">The client registration request, <see cref="ClientRegistrationStartResult.registrationRequest"/> </param>
     /// <param name="username">The username to register</param>
     /// <returns></returns>
     public ServerRegistrationStartResult StartRegistration(CipherConfiguration config, byte[]? serverSetup, byte[] registrationRequest, string username)
     {
-        var (registrationResponse, serverSetupRet) = BitwardenLibrary.StartServerRegistration(config, serverSetup, registrationRequest, username);
+        var result = BitwardenLibrary.ExecuteFFIFunction((ffi) =>
+        {
+            return BitwardenLibrary.start_server_registration(ffi.Cfg(config), ffi.Buf(serverSetup), ffi.Buf(registrationRequest), username);
+        }, 2);
+
         return new ServerRegistrationStartResult
         {
-            registrationResponse = registrationResponse,
-            serverSetup = serverSetupRet
+            registrationResponse = result[0],
+            serverSetup = result[1]
         };
     }
 
     /// <summary>
-    /// Finish the server registration process. This must happen after <see cref="Bitwarden.Opaque.BitwardenOpaqueClient.FinishRegistration"/> 
+    /// Finish the server registration process. This must happen after <see cref="BitwardenOpaqueClient.FinishRegistration"/> 
     /// </summary>
     /// <param name="config">The Cipher configuration, must be the same for all the operation</param>
-    /// <param name="registrationUpload">The client registration upload, <see cref="Bitwarden.Opaque.ClientRegistrationFinishResult.registrationUpload"/> </param>
+    /// <param name="registrationUpload">The client registration upload, <see cref="ClientRegistrationFinishResult.registrationUpload"/> </param>
     /// <returns></returns>
     public ServerRegistrationFinishResult FinishRegistration(CipherConfiguration config, byte[] registrationUpload)
     {
-        var serverRegistration = BitwardenLibrary.FinishServerRegistration(config, registrationUpload);
+        var result = BitwardenLibrary.ExecuteFFIFunction((ffi) =>
+        {
+            return BitwardenLibrary.finish_server_registration(ffi.Cfg(config), ffi.Buf(registrationUpload));
+        }, 1);
         return new ServerRegistrationFinishResult
         {
-            serverRegistration = serverRegistration
+            serverRegistration = result[0]
         };
     }
 
+    /// <summary>
+    /// Start the server login process. This must happen after <see cref="BitwardenOpaqueClient.StartLogin"/> 
+    /// </summary>
+    /// <param name="config">The Cipher configuration, must be the same for all the operation</param>
+    /// <param name="serverSetup">The server setup, previously generated or supplied during registration</param>
+    /// <param name="serverRegistration">The server registration, previously generated during registration</param>
+    /// <param name="credentialRequest">The client credential request, <see cref="ClientLoginStartResult.credentialRequest"/> </param>
+    /// <param name="username">The username to login</param>
+    /// <returns></returns>
     public ServerLoginStartResult StartLogin(CipherConfiguration config, byte[] serverSetup, byte[] serverRegistration, byte[] credentialRequest, string username)
     {
-        var (credentialResponse, state) = BitwardenLibrary.StartServerLogin(config, serverSetup, serverRegistration, credentialRequest, username);
+        var result = BitwardenLibrary.ExecuteFFIFunction((ffi) =>
+        {
+            return BitwardenLibrary.start_server_login(ffi.Cfg(config), ffi.Buf(serverSetup), ffi.Buf(serverRegistration), ffi.Buf(credentialRequest), username);
+        }, 2);
         return new ServerLoginStartResult
         {
-            credentialResponse = credentialResponse,
-            state = state
+            credentialResponse = result[0],
+            state = result[1]
         };
     }
 
+    /// <summary>
+    /// Finish the server login process. This must happen after <see cref="BitwardenOpaqueClient.FinishLogin"/> 
+    /// </summary>
+    /// <param name="config">The Cipher configuration, must be the same for all the operation</param>
+    /// <param name="state">The state generated during the login start, <see cref="ServerLoginStartResult.state"/> </param>
+    /// <param name="credentialFinalization">The client credential finalization, <see cref="ClientLoginFinishResult.credentialFinalization"/> </param>
+    /// <returns></returns>
     public ServerLoginFinishResult FinishLogin(CipherConfiguration config, byte[] state, byte[] credentialFinalization)
     {
-        var sessionKey = BitwardenLibrary.FinishServerLogin(config, state, credentialFinalization);
+        var result = BitwardenLibrary.ExecuteFFIFunction((ffi) =>
+        {
+            return BitwardenLibrary.finish_server_login(ffi.Cfg(config), ffi.Buf(state), ffi.Buf(credentialFinalization));
+        }, 1);
         return new ServerLoginFinishResult
         {
-            sessionKey = sessionKey
+            sessionKey = result[0]
         };
     }
 
