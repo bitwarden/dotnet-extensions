de64a4f03831631f1447be379fa7a932f27e6870
Justin Baur <19896123+justindbaur@users.noreply.github.com>
2026-06-25 14:55:39 -0400

    Formatting

--- extensions/Bitwarden.Server.Sdk.WebEssentials/src/WebEssentialsEndpointRouteBuilderExtensions.cs
+++ extensions/Bitwarden.Server.Sdk.WebEssentials/src/WebEssentialsEndpointRouteBuilderExtensions.cs
@@ -1,5 +1,4 @@
 using System.Reflection;
-using Bitwarden.Server.Sdk.WebEssentials;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
